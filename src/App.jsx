b93b22808cdfb010173250fa34b883f793e6ee2d
Syed Ahamed Meeran <143245064+Syedmeeran14@users.noreply.github.com>
2024-01-08 23:47:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import Navbar from './components/Navbar'
-import Header from './components/header'
+import Header from './components/Header'
 import Card from './components/Card'
 import Footer from './components/Footer'
 
